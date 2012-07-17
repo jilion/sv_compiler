6347047c8125a85f88ad2c082d251c71cd9e734e
Andrea Coiro <andrea.coiro@gmail.com>
2012-07-17 18:43:16 +0200

    Lots of fixes (beta commit).

--- uglify-js.js
+++ uglify-js.js
@@ -4,13 +4,21 @@ function uglify(orig_code, options){
   var jsp = uglify.parser;
   var pro = uglify.uglify;
 
+
+  options.sv_options.mangle = true;
+  // orig_code = "var _css; _css = { fontSize: 3 }"
+
+  // orig_code = "var _ = { embed: top !== self }"
+  // orig_code = "function test(fs) { var window = {}; window.isFullscreen() }"
+  // orig_code = "AClass.prototype.test = function(fs) { var window = {}; window.isFullscreen() }"
+
   var ast = jsp.parse(orig_code, options.strict_semicolons); // parse code and get the initial AST
-  
-  ast = pro.ast_logger(ast, options.sv_options); // get a new AST with logging messages stripped  
-  ast = pro.ast_aggressive_mangle(ast, options.sv_options); // get a new AST with super mangled names
+  // ast = pro.ast_logger(ast, options.sv_options); // get a new AST with logging messages stripped
   if (options.sv_options && options.sv_options.mangle) {
+    ast = pro.ast_mangle_string(ast, options.sv_options); // get a new AST with mangled strings wrapped by the MANGLE() macro
+    ast = pro.ast_aggressive_mangle(ast, options.sv_options); // get a new AST with super mangled names
     ast = pro.ast_mangle(ast, options.mangle_options); // get a new AST with mangled names
-    ast = pro.ast_squeeze(ast, options.squeeze_options); // get an AST with compression optimizations  
+    ast = pro.ast_squeeze(ast, options.squeeze_options); // get an AST with compression optimizations
   }
 
   var final_code = pro.gen_code(ast, options.gen_options); // compressed code here
