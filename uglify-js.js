7dc6e07a8816ad73ad04c715e7c43a111162c60b
Andrea Coiro <andrea@andrea.local>
2012-10-25 11:18:15 +0200

    Enabled logs stripper.

--- uglify-js.js
+++ uglify-js.js
@@ -19,11 +19,19 @@ function uglify(orig_code, options){
   // orig_code = 'sublime_.module("Tz", [], function() {var ADD_COLOR_STOP; if (window[a]) {ADD_COLOR_STOP=3}})'
   var ast = jsp.parse(orig_code, options.strict_semicolons); // parse code and get the initial AST
   // ast = pro.ast_logger(ast, options.sv_options); // get a new AST with logging messages stripped
+
+  // ast = pro.ast_logger(ast, {log_level:'fatal'}); // get an AST with compression optimizations
+
+
+  if (options.sv_options &&  options.sv_options.removeLogs) {
+    ast = pro.ast_logger(ast, {log_level:'fatal'}); // get an AST with compression optimizations
+  }
   if (options.sv_options && options.sv_options.mangle) {
     ast = pro.ast_mangle_string(ast, options.sv_options); // get a new AST with mangled strings wrapped by the MANGLE() macro
     ast = pro.ast_aggressive_mangle(ast, options.sv_options); // get a new AST with super mangled names
     ast = pro.ast_mangle(ast, options.mangle_options); // get a new AST with mangled names
     ast = pro.ast_squeeze(ast, options.squeeze_options); // get an AST with compression optimizations
+
   }
 
   var final_code = pro.gen_code(ast, options.gen_options); // compressed code here
